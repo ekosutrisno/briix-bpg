b430563b0dd94cc99c8c8c70c4b177ae844ad730
Eko Sutrisno <eko@briix.com>
2022-08-21 17:49:55 +0700

    init: add all assets BPG Web

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="max-w-[1440px] mx-auto">
+      <Hero/>
+
+      <Section1/>
+
+      <Section2/>
+
+      <Section3/>
+
+      <Section4/>
+
+      <Footer/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Hero from './components/Hero.vue';
+import Section1 from './components/Section1.vue';
+import Section2 from './components/Section2.vue';
+import Section3 from './components/Section3.vue';
+import Footer from './components/Footer.vue';
+import Section4 from './components/Section4.vue';
+
+</script>
+
+<style scoped>
+
+</style>
